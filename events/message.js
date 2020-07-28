6f5fb79746c94be8539a72639f41099a07c79735
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-07-28 13:11:25 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 module.exports = (client, message) => {
     if (message.author.bot) return;
 
-    const prefix = `${client.config.prefix} `;
+    const prefix = `/ `;
     if (!message.content.toLowerCase().includes(prefix)) return;
     else {
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
