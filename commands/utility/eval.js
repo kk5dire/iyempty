be32af695421f8dee083bfda3f31bd27b8fee770
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-07-28 14:39:28 -0400

    Create eval.js

---
+++ commands/utility/eval.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+    try {
+          const output = eval(args.join(' '));
+           console.log(`evalulated javascript command line |  "${args}"`);
+    }catch(err) {
+        console.log('Error here');
+        message.channel.send("error in channel here");
+    }
+}
+    
+exports.help = {
+        enabled: true,
+        hideHelp: false,
+        type: "util",
+        name: "eval",
+        description: "run js code)",
+        usage: "`/ eval message.channel.send(\"hi\");`",
+}
